49f76a46f2670695bd0ed5da07d756f0250f7de3
progamer9138 <39977408+progamer9138@users.noreply.github.com>
2022-01-16 10:59:05 -0800

    11 WA

--- 11.py
+++ 11.py
@@ -1,13 +1,31 @@
+def topTwo(array):
+    top1 = array[0]
+    top2 = array[0]
+    top1Pos = 0
+    top2Pos = 0
+    for i in range(1,len(array)):
+        if array[i] > top1:
+            top2 = top1
+            top2Pos = top1Pos
+            top1 = array[i]
+            top1Pos = i
+        elif array[i] > top2:
+            top2 = array[i]
+            top2Pos = i
+    return top1Pos, top2Pos
+
 def maxArea(height):
     topArea = min(height[0],height[-1]) * (len(height)-1)
     hLen = len(height)
+    topHeights = topTwo(height)
+    print(topHeights[0], topHeights[1])
     for i in reversed(range(2, hLen)):
-        for j in range(hLen-i+1):
+        for j in range(hLen-i):
+            print(j, i+j)
+            if j > topHeights[0] and i+j < topHeights[1]:
+                continue
             #make efficent
-            curr = height[j:i+j]
-            val1 = curr[0]
-            val2 = curr[-1]
-            area = min(val1,val2) * (i-1)
+            area = min(height[j], height[i+j]) * (i)
             if area > topArea:
                 topArea = area
     return topArea
