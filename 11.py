6e09e303276407a23c0d013253c8c25001773a29
progamer9138 <39977408+progamer9138@users.noreply.github.com>
2022-01-16 10:24:08 -0800

    11 TLE

--- 11.py
+++ 11.py
@@ -1,10 +1,13 @@
 def maxArea(height):
-    topArea = 0
-    for i in range(len(height)):
-        for j in range(i, len(height)):
-            if i == j:
-               continue
-            area = min(height[i], height[j]) * (j - i)
+    topArea = min(height[0],height[-1]) * (len(height)-1)
+    hLen = len(height)
+    for i in reversed(range(2, hLen)):
+        for j in range(hLen-i+1):
+            #make efficent
+            curr = height[j:i+j]
+            val1 = curr[0]
+            val2 = curr[-1]
+            area = min(val1,val2) * (i-1)
             if area > topArea:
                 topArea = area
     return topArea
@@ -12,3 +15,4 @@ def maxArea(height):
 ins = input()
 height = [int(i) for i in ins.split(',')]
 print(maxArea(height))
+#1, 2, 3, 4, 6, 9, 12, 18, 36
