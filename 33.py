c861425a1851b9722b035a47a0ecc82de2078d3d
progamer9138 <39977408+progamer9138@users.noreply.github.com>
2022-02-08 18:03:12 -0800

    Leetcode 33

--- 33.py
+++ 33.py
@@ -1,25 +1,31 @@
-from re import L
-
-
 def search(nums, target):
     first = nums[0]
     left, right = 0, len(nums)-1
     curr = int(len(nums)/2)
     if first == target:
         return 0
-    while nums[curr] != target:
+    if nums[-1] == target:
+        return len(nums)-1
+    while nums[curr] != target and left < right-1:
         if target < first:
             if nums[curr] > first:
+                left = curr
                 curr += int((right-curr)/2)
             elif nums[curr] < target:
+                left = curr
                 curr += int((right-curr)/2)
             else:
+                right = curr
                 curr -= int((curr-left)/2)
-        elif nums[curr] > target:
+        elif nums[curr] > target or nums[curr] < first:
+            right = curr
             curr -= int((curr-left)/2)
         else:
+            left = curr
             curr += int((right-curr)/2)
-    return curr
+    if nums[curr] == target:
+        return curr
+    return -1
 
 ins = input().strip('[]').split(',')
 target = int(input())
@@ -29,6 +35,4 @@ print(search(inArr, target))
 """
 curr += int((right-curr)/2) #increase
 curr -= int((curr-left)/2) #decrease
-
-
 """
