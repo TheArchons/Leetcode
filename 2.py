21073d7e88566987a09f76d190562c86fe62cb9b
TheArchons <progamer9138@gmail.com>
2022-03-21 17:46:50 -0700

    2 partial

--- 2.py
+++ 2.py
@@ -6,20 +6,52 @@ class ListNode:
 
 class Solution:
     def carry(self, l1, pos): #carry the 1 to the next node
+        l1Head = l1
         for i in range(pos-1):
-            l1 = l1.next
-        l1.val += 1
-        if l1.val >= 10:
-            l1.val -= 10
-            self.carry(l1, pos)
+            l1Head = l1Head.next
+        l1Head.val += 1
+        if l1Head.val >= 10:
+            l1Head.val -= 10
+            self.carry(l1, pos-1)
 
     def addTwoNumbers(self, l1, l2):
         l1Head = l1
         l2Head = l2
         pos = 0
 
+        #!start l1 at the start of l2
+        #!carrying the first node
+
+        #find the length of l1 and l2
+        l1Len = 0
+        l2Len = 0
+        while l1Head:
+            l1Len += 1
+            l1Head = l1Head.next
+        while l2Head:
+            l2Len += 1
+            l2Head = l2Head.next
+        
+        #reset l1Head and l2Head
+        l1Head = l1
+        l2Head = l2
+
+        #if l2 is longer than l1, swap l1 and l2
+        if l2Len > l1Len:
+            l1Head, l2Head = l2Head, l1Head
+            l1Len, l2Len = l2Len, l1Len
+
+        #if l1 is longer than l2, start l1 at the start of l2
+        if l1Len > l2Len:
+            for i in range(l1Len - l2Len):
+                l1Head = l1Head.next
+                pos += 1
+
         #add the first node
-        l1.val += l2.val
+        l1Head.val += l2Head.val
+        if l1Head.val >= 10:
+            l1Head.val -= 10
+            self.carry(l1, pos)
 
         while l2Head.next != None: #add the rest of the nodes
             pos += 1
@@ -54,6 +86,9 @@ class Solution:
 
         return prev
 
+
+
+#testing code
 list1 = input().strip('[]').split(',')
 list2 = input().strip('[]').split(',')
 l1 = ListNode(int(list1[0]))
