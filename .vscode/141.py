c97b42904e16eb88aad5d83fc144df0dbdeee770
TheArchons <progamer9138@gmail.com>
2022-03-23 13:04:18 -0700

    141 using Floyd's tortoise and hare

--- .vscode/141.py
+++ .vscode/141.py
@@ -1,11 +1,14 @@
 # Definition for singly-linked list.
+from statistics import harmonic_mean
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 #put leetcode code here
-class Solution:
+"""class Solution: #O(n) time, O(n) space
     def hasCycle(self, head) -> bool:
         visited = set()
         while head:
@@ -13,6 +16,19 @@ class Solution:
                 return True
             visited.add(head)
             head = head.next
+        return False"""
+
+class Solution:
+    def hasCycle(self, head) -> bool: #tortoise and hare algorithm
+        if not head or not head.next:
+            return False
+
+        tortoise, hare = head, head.next
+        while hare and hare.next:
+            tortoise = tortoise.next
+            hare = hare.next.next
+            if tortoise == hare:
+                return True
         return False
 
 #debug code
@@ -26,13 +42,14 @@ for i in range(1,len(list1)):
     head.next = ListNode(list1[i])
     head = head.next
 
-end = head
 pos = int(input())
-head = l1
-#travel to pos
-for i in range(pos):
-    head = head.next
-end.next = head
+if pos != -1:
+    end = head
+    head = l1
+    #travel to pos
+    for i in range(pos):
+        head = head.next
+    end.next = head
 
 result = Solution.hasCycle(Solution, l1)
 
