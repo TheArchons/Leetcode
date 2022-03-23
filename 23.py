76cf9bba83310193f99cdd9c437e20580094a557
TheArchons <progamer9138@gmail.com>
2022-03-22 22:02:52 -0700

    23 partial

---
+++ 23.py
@@ -0,0 +1,74 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+#put leetcode code here
+class Solution:
+    def mergeTwoLists(self, list1, list2): #code from 21.py
+        l1Head = list1
+        l2Head = list2
+        outputList = ListNode()
+        outputListHead = outputList
+        while l1Head or l2Head:
+            if l1Head:
+                if not l2Head or l1Head.val <= l2Head.val:
+                    outputListHead.val = l1Head.val
+                    outputListHead.next = ListNode()
+                    outputListHead = outputListHead.next
+                    l1Head = l1Head.next
+                    continue
+                else:
+                    outputListHead.val = l2Head.val
+                    outputListHead.next = ListNode()
+                    outputListHead = outputListHead.next
+                    l2Head = l2Head.next
+                    continue
+            elif l2Head:
+                outputListHead.val = l2Head.val
+                outputListHead.next = ListNode()
+                outputListHead = outputListHead.next
+                l2Head = l2Head.next
+                continue
+
+        #remove the last node
+        head = outputList
+        while head and head.next != outputListHead:
+            head = head.next
+        head.next = None
+        return outputList
+
+    def mergeKLists(self, lists):
+        if not lists: #if empty list
+            return None
+        startList = lists[0]
+        for i in range(1, len(lists)):
+            if not lists[i]: #if the list is empty, skip it
+                continue
+            startList = self.mergeTwoLists(self, startList, lists[i])
+        return startList
+
+
+#debug code
+
+ins = input().split('],')
+lists = []
+
+for l in ins:
+    list1 = l.strip('[]').split(',')
+    list1 = [int(i) for i in list1]
+
+    l1 = ListNode(list1[0])
+    head = l1
+    for i in range(1,len(list1)):
+        head.next = ListNode(list1[i])
+        head = head.next
+    lists.append(l1)
+
+result = Solution.mergeKLists(Solution, lists)
+
+#print result
+while result != None:
+    print(result.val)
+    result = result.next
